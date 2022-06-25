cbb17b49e6af60a12e2481a3eddb855eabff68b3
qndn3tp <qndn3tp@gmail.com>
2022-06-25 18:33:22 +0900

    feat: 사용자 주위 반경 표시

--- frontend/src/components/Map/kakaoMap.js
+++ frontend/src/components/Map/kakaoMap.js
@@ -18,6 +18,20 @@ export default function KakaoMapScript() {
         position: markerPosition,
         image: markerImage 
     });
+    // 지도에 표시할 원 생성(사용자의 주소로부터 범위)
+    const circle = new kakao.maps.Circle({
+    center : new kakao.maps.LatLng(37.610632352771624, 126.99701001706818),  // 원의 중심좌표
+    radius: 150, // 미터 단위
+    strokeWeight: 5, // 선의 두께 
+    strokeColor: '#75B8FA', // 선의 색깔
+    strokeOpacity: 1, // 선의 불투명도
+    strokeStyle: 'dashed', // 선의 스타일
+    fillColor: '#CFE7FF', // 채우기 색깔
+    fillOpacity: 0.5  // 채우기 불투명도   
+}); 
+
+// 지도에 원을 표시합니다 
+circle.setMap(map); 
 
     marker.setMap(map); 
 

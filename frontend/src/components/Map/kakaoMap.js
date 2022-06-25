62b99ef38eba7a7e63860e928d0f5067bf1bde80
qndn3tp <qndn3tp@gmail.com>
2022-06-25 17:38:01 +0900

    feat: add kakaomap comment

--- frontend/src/components/Map/kakaoMap.js
+++ frontend/src/components/Map/kakaoMap.js
@@ -1,10 +1,10 @@
 const { kakao } = window;
 
 export default function KakaoMapScript() {
-    const container = document.getElementById('myMap');
+    const container = document.getElementById('myMap');    // 지도를 담을 영역의 DOM 레퍼런스
     const options = {
-        center: new kakao.maps.LatLng(33.450701, 126.570667),
-        level: 3
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3                                              // 지도의 레벨(확대, 축소 정도)
     };
     const map = new kakao.maps.Map(container, options);
 
